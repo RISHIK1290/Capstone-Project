bb78b836ebe59634e176259f1a2723540ce757b3
RISHIK1290 <118056509+RISHIK1290@users.noreply.github.com>
2023-08-13 12:24:58 +0530

    new changes

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -33,7 +34,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use("*", (req, res) => {
+app.use('*',function(req,res){
   res.sendFile(path.join(__dirname,'./client/build/index.html'))
 });
 
